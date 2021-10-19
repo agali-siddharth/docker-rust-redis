ed4363d1d345c530493855d2af29225630640e34
agali-siddharth <agali.siddharth@gmail.com>
2021-10-19 05:35:27 +0000

    made code modular, added test for redis set-and-get

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,85 @@
+extern crate redis;
+
+use redis::Commands;
+use redis::RedisResult;
+use std::env;
+
+#[derive(Debug)]
+enum Command {
+  Set,
+  Get,
+}
+
+fn set_to_redis(key :String, value :String) -> Result<(), String> {
+  let client = redis::Client::open("redis://127.0.0.1/").unwrap();
+  let mut con = client.get_connection().unwrap();
+
+  let ret :RedisResult<()> = con.set(key, value);
+  match ret {
+    Ok(_) => Ok(()),
+    _ => Err(format!("error in writing to db!")),
+  }
+}
+
+fn get_from_redis(key: String) -> Result<String, String> {
+  let client = redis::Client::open("redis://127.0.0.1/").unwrap();
+  let mut con = client.get_connection().unwrap();
+
+  let ret :RedisResult<String> = con.get(key);
+  match ret {
+    Ok(x) => {println!("obtained - {}", x); Ok(x)},
+    _ => {return Err(format!("error in getting from db!"))},
+  }
+}
+
+fn process_set(args :&mut env::Args) -> Result<(), String> {
+  if args.len() != 4 {
+    return Err(format!("wrong number of arguments for set command : {}", args.len()));
+  }
+
+  let key = args.nth(2).unwrap();
+  // nth() consumes all preceding elements as well as the current one. so the next argument to nth should be 0
+  let value = args.nth(0).unwrap();
+
+  set_to_redis(key,value)
+}
+
+fn process_get(args :&mut env::Args) -> Result<String, String> {
+  if args.len() != 3 {
+    return Err(format!("wrong number of arguments for get command : {}", args.len()));
+  }
+  let key = args.nth(2).unwrap();
+
+  get_from_redis(key)
+}
+
+pub fn parse_args() -> Result<(), String> {
+  let mut args = env::args();
+  let mut orig_args = env::args();
+  if args.len() < 2 {
+    return Err(format!("wrong number of arguments : {}", args.len()));
+  }
+  let command = args.nth(1).unwrap();
+  let cmd = match command.as_str() {
+    "set" => Command::Set,
+    "get" => Command::Get,
+    _ => { return Err(format!("invalid command {}", command)); },
+  };
+
+  match cmd {
+    Command::Set => {process_set(&mut orig_args)?;},
+    Command::Get => {process_get(&mut orig_args)?;},
+  }
+
+  Ok(())
+}
+
 #[cfg(test)]
 mod tests {
-    fn adder(a :i32, b :i32) -> i32 {
-        a + b
-    }
+use crate::*;
     #[test]
-    fn it_works() {
-        assert_eq!(adder(3,2), 5);
+    fn test_set_and_get() {
+        assert_eq!(set_to_redis("test_val".to_string(), 100.to_string()).is_ok(), true);
+        assert_eq!(get_from_redis("test_val".to_string()).unwrap(), 100.to_string());
     }
 }
